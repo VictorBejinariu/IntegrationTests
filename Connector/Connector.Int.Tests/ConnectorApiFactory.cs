ab4b1a61a95e60c52156b8fc048fe5eb2aaf8bd8
Victor Bejinariu <Victor.Bejinariu@nuvei.com>
2023-01-07 18:59:57 +0200

    Do integration Test of Health Check

---
+++ Connector/Connector.Int.Tests/ConnectorApiFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+
+namespace Connector.Int.Tests;
+
+public class ConnectorApiFactory :WebApplicationFactory<IApiMarker>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+        });
+    }
+}
--- Connector/Connector/Controllers/HealthController.cs
+++ Connector/Connector/Controllers/HealthController.cs
@@ -6,9 +6,17 @@ namespace Connector.Controllers;
 [Route("[controller]")]
 public class HealthController:ControllerBase
 {
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger??throw new ArgumentNullException(nameof(logger));
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
+        _logger.LogInformation($"{nameof(HealthController)}:{nameof(Get)}");
         return Ok();
     }
 }
