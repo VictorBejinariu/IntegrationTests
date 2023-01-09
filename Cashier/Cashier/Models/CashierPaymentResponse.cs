8bc1fdf03f22c5fda69a5a53d03256b733526c95
Victor Bejinariu <Victor.Bejinariu@nuvei.com>
2023-01-09 09:36:21 +0200

    Added basic cashier payment action

---
+++ Cashier/Cashier/Models/CashierPaymentResponse.cs
@@ -0,0 +1,8 @@
+namespace Cashier.Controllers;
+
+public class CashierPaymentResponse 
+{
+    public string ClientUniqueId { get; set; }
+    public string MerchantId { get; set; }
+    public string SiteId { get; set; }
+}
